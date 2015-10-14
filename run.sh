a40e18ff0c6bb5c3cb1f184b874b167e64599245
Stas Kelvich <stas.kelvich@gmail.com>
2015-10-14 16:13:56 +0300

    fix hostnames

--- run.sh
+++ run.sh
@@ -1,33 +1,30 @@
-blade10:
+astro10:
 	mkdir -p /tmp/memcockroach
 	sudo mount -t tmpfs -o size=512M tmpfs /tmp/memcockroach/
 	./cockroach init --stores=hdd="/tmp/memcockroach"
 	./cockroach start --stores=hdd="/tmp/memcockroach" --addr=":26257" --gossip="astro9:26257,astro8:26257,astro6:26257,astro5:26257,astro4:26257"
 
-blade9:
+astro9:
 	mkdir -p /tmp/memcockroach
 	sudo mount -t tmpfs -o size=512M tmpfs /tmp/memcockroach/
 	./cockroach start --stores=hdd="/tmp/memcockroach" --addr=":26257" --gossip="astro10:26257,astro8:26257,astro6:26257,astro5:26257,astro4:26257"
 
-blade9:
+astro8:
 	mkdir -p /tmp/memcockroach
 	sudo mount -t tmpfs -o size=512M tmpfs /tmp/memcockroach/
 	./cockroach start --stores=hdd="/tmp/memcockroach" --addr=":26257" --gossip="astro9:26257,astro10:26257,astro6:26257,astro5:26257,astro4:26257"
 
-blade9:
+astro6:
 	mkdir -p /tmp/memcockroach
 	sudo mount -t tmpfs -o size=512M tmpfs /tmp/memcockroach/
 	./cockroach start --stores=hdd="/tmp/memcockroach" --addr=":26257" --gossip="astro9:26257,astro8:26257,astro10:26257,astro5:26257,astro4:26257"
 
-
-blade9:
+astro5:
 	mkdir -p /tmp/memcockroach
 	sudo mount -t tmpfs -o size=512M tmpfs /tmp/memcockroach/
 	./cockroach start --stores=hdd="/tmp/memcockroach" --addr=":26257" --gossip="astro9:26257,astro8:26257,astro6:26257,astro10:26257,astro4:26257"
 
-
-blade9:
+astro4:
 	mkdir -p /tmp/memcockroach
 	sudo mount -t tmpfs -o size=512M tmpfs /tmp/memcockroach/
 	./cockroach start --stores=hdd="/tmp/memcockroach" --addr=":26257" --gossip="astro9:26257,astro8:26257,astro6:26257,astro5:26257,astro10:26257"
-
